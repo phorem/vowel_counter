2941893ddd79f7d8bc347b9283489e908b391c0a
phorem <88844280+phorem@users.noreply.github.com>
2022-10-27 09:36:44 -0600

    Update vowel_counter.py

--- vowel_counter.py
+++ vowel_counter.py
@@ -1,6 +1,7 @@
 #word = input("what word would you like to count vowels in? ").lower()
 #vowels = ["a", "e", "i", "o", "u"]
 
+#Made a function to take a word input from a user and count how many vowels that it has.
 
 def vowel_count(word, vowels=("a","e","i","o","u")):
     counter = 0
